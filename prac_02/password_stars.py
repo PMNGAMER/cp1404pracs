63305b7cdd567838c2a6651d1259044a46a8decb
my_name <myintnaing.phone@my.jcu.edu.au>
2025-06-01 19:34:49 +0800

    Adding Functions with Refactor tool

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,8 +1,18 @@
 MIN_LENGTH = 6
 
-password = input("Enter a password: ")
-while len(password) < MIN_LENGTH:
-    print(f"Password is too short! Must be at least {MIN_LENGTH} characters.")
+def main():
+    password = get_password(MIN_LENGTH)
+    print_asterisks(password)
+def get_password(MIN_LENGTH):
     password = input("Enter a password: ")
+    while len(password) < MIN_LENGTH:
+        print(f"Password is too short! Must be at least {MIN_LENGTH} characters.")
+        password = input("Enter a password: ")
+    return password
 
-print("*" * len(password))
+
+def print_asterisks(password):
+    print("*" * len(password))
+
+main()
+
